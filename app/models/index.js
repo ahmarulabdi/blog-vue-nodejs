6ea31f43aea3d01cfa3e4306646282651c3863e8
ahmarulabdi <rndmjck@gmail.com>
2020-05-25 23:09:16 +0700

    fix bug model index

--- app/models/index.js
+++ app/models/index.js
@@ -3,12 +3,11 @@ const Sequelize = require('sequelize')
 
 const sequelizeConfig = new Sequelize(dbConfig.db, dbConfig.user, dbConfig.password, {
     host: dbConfig.host,
-    dialect: dbConfig.dialect,
-    operatorsAliases: false
+    dialect: dbConfig.dialect
 })
 
 const db = {}
 db.Sequelize = Sequelize
 db.sequelizeConfig = sequelizeConfig
 
-db.tutorial = require('../models/tutorial.model')(sequelizeConfig, Sequelize)
+db.tutorial = require('./tutorial.model')(sequelizeConfig, Sequelize)
