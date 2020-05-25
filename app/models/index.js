f982198dafed9a71eb79a8eb0cd4ed03980c19f7
ahmarulabdi <rndmjck@gmail.com>
2020-05-25 22:46:26 +0700

    registration sequelize config

---
+++ app/models/index.js
@@ -0,0 +1,12 @@
+const dbConfig = require('../config/db.config')
+const Sequelize = require('sequelize')
+
+const sequelizeConfig = new Sequelize(dbConfig.db, dbConfig.user, dbConfig.password, {
+    host: dbConfig.host,
+    dialect: dbConfig.dialect,
+    operatorsAliases: false
+})
+
+const db = {}
+db.Sequelize = Sequelize
+db.sequelizeConfig = sequelizeConfig
