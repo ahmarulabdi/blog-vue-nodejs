666be9cb1570bc2a8a95ee670e545ac032b9f16f
ahmarulabdi <rndmjck@gmail.com>
2020-05-25 22:52:46 +0700

    define prop tutorial model

--- app/models/tutorial.model.js
+++ app/models/tutorial.model.js
@@ -1,3 +1,13 @@
 module.exports = (sequelizeConfig, Sequelize) => {
-
+    return sequelizeConfig.define('tutorial', {
+        title: {
+            type: Sequelize.STRING,
+        },
+        description: {
+            type: Sequelize.STRING
+        },
+        published: {
+            type: Sequelize.STRING
+        }
+    })
 }
